85b9d8b9763b16cd3c5648cc7d15ebad4d06f73b
Isa Vega <imvega100@gmail.com>
2023-09-20 19:42:59 -0400

    changed category and tag routes

--- models/index.js
+++ models/index.js
@@ -5,24 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
+Product.belongsTo(Category,{
   foreignKey: "category_id"
 })
 
 // Categories have many Products
-Category.hasMany(Product, {
+Category.hasMany(Product,{
   foreignKey:"category_id",
   onDelete: "CASCADE",
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
+Product.belongsToMany(Tag,{
   through:ProductTag,
   foreignKey: "product_id"
 })
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
+Tag.belongsToMany(Product,{
   through:ProductTag,
   foreignKey: "tag_id"
 })
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,27 +2,83 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+//http://localhost:3001/api/categories
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    })
+    res.status(200).json(categoryData)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+//http://localhost:3001/api/categories/1
+
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+
+  try {
+    const id = req.params.id
+    const categoryData = await Category.findByPk(id, {
+      include: [{ model: Product }]
+    })
+    res.status(200).json(categoryData)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
+//http://localhost:3001/api/categories
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body)
+    res.status(200).json(categoryData)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+//http://localhost:3001/api/categories/1
+//put is update
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id:req.params.id
+      }
+    })
+    res.status(200).json(categoryData)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+//http://localhost:3001/api/categories/1
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id:req.params.id
+      }
+    })
+    res.status(200).json(categoryData)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
